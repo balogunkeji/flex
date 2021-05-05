b7dd29be2b6c349a5c4ac4ec33e8ce4d09a99f6d
keji <tomitoyo.93@gmail.com>
2021-05-05 11:55:03 +0100

    commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import hijab from './img/hijab.jpg';
+import hijab2 from './img/hijab2.jpg';
+
+const Home = () => {
+    return ( 
+        <div className='container'>
+            
+        </div>
+     );
+}
+ 
+export default Home;
